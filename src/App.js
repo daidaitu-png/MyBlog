76b0a295e46a84aba9b125defcb07f9676c36f2e
liuming <15926724726@163.com>
2022-03-05 21:35:14 +0800

    banner&courses模块

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "normalize.css";
+import Home from './container/Home'
+import "./style.scss";
+
+// import styles from "./style.module.scss";
+
+function App() {
+	return <Home />;
+}
+
+export default App;
---
+++ src/container/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Bannner from './components/Banner';
+import Courses from './components/Courses';
+
+const Home = () => {
+  return (
+    <div>
+      <Bannner/>
+      <Courses/>
+    </div>
+  );
+}
+
+export default Home;
